f1a98270b036110ff5774d62572bb9203cacd23c
HFMUHFZKNM7LMLRJ <1363019064@qq.com>
2025-01-09 16:18:15 +0800

    test final

--- helloGit.cpp
+++ helloGit.cpp
@@ -1,11 +1,48 @@
-#include <iostream>
+//Coded By Millet_Congee ^~^_^~^_*.*_*.*_*.*_*.*@____O_O____@*.*_*.*_*.*_*.*_^~^_^~^
+#include<bits/stdc++.h>
+#define LL long long
+#define PII pair<int,int>
+#define fr first
+#define sd second
+#define pb push_back
+#define endl '\n'
+#define i128 __int128 
+#define lc p<<1
+#define rc p<<1|1
+using namespace std;
+const int N=2e5+10;
+const int INF=0x3f3f3f3f;
+const int MOD = 998244353;
+int dx[4]={0,1,0,-1},dy[4]={1,0,-1,0};
 
+#ifdef _IGNORE_
+inline LL read()  
+{
+    int x=0,f=1;char ch=getchar();
+    while (ch<'0'||ch>'9'){if (ch=='-') f=-1;ch=getchar();}
+    while (ch>='0'&&ch<='9'){x=x*10+ch-48;ch=getchar();}
+    return x*f;
+}
+
+inline void write(LL x){ 
+    if(x<0) putchar('-'), x=-x;
+    if(x>9) write(x/10);
+    putchar(x%10 +48);
+}
+#endif
+
+//Notice if it needs to be turned on LL;
+//Note whether you need to set the data to zero each time;
+
+void solve(){
+    
+}
 
 int main(){
-    int a;
-
-    std::cout<<"maybe successed!"<<std::endl;
-
-    std::cout<<"hello git"<<std::endl;
-    return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+        
+       solve();
+    
 }
